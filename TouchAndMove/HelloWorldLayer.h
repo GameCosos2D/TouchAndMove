27f18bd4fe308e4f5176cb08612de54d15d3a234
Marco Velluto <velluto93@gmail.com>
2013-08-10 00:28:39 +0200

    Initial Commit

---
+++ TouchAndMove/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  TouchAndMove
+//
+//  Created by Marco Velluto on 10/08/13.
+//  Copyright Algos 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ TouchAndMove/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TouchAndMove
+//
+//  Created by Marco Velluto on 10/08/13.
+//  Copyright Algos 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
